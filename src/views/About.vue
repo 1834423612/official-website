8bc5b383d38e574f152110ad3c8515aa052b6e88
1834423612 <1834423612@qq.com>
2024-08-05 20:38:05 -0400

    fix: (Mobile) About Page Team Stats Value flex layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -15,7 +15,7 @@
     <div class="mx-auto mt-12 max-w-7xl px-12">
       <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
         <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our mission</h2>
-        <div class="mt-6 flex flex-col gap-x-8 gap-y-20 lg:flex-row">
+        <div class="mt-6 flex flex-col gap-x-8 gap-y-10 lg:gap-y-20 lg:flex-row">
           <div class="lg:w-full lg:max-w-2xl lg:flex-auto">
             <p class="text-xl leading-8 text-gray-600">Some words.......................</p>
             <div class="mt-10 max-w-xl text-base leading-7 text-gray-700">
@@ -28,9 +28,9 @@
               </p>
             </div>
           </div>
-          <div class="lg:flex lg:flex-auto lg:justify-center">
+          <div class="flex flex-col items-center lg:flex-auto lg:justify-center">
             <dl class="w-64 space-y-8 xl:w-80">
-              <div v-for="stat in stats" :key="stat.label" class="flex flex-col-reverse gap-y-4">
+              <div v-for="stat in stats" :key="stat.label" class="flex flex-col-reverse gap-y-4 items-center">
                 <dt class="text-base leading-7 text-gray-600">{{ stat.label }}</dt>
                 <dd class="text-5xl font-semibold tracking-tight text-gray-900">{{ stat.value }}</dd>
               </div>
